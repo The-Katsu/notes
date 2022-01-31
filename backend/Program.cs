7711770a3d8cb168e35c216d698ac9e516f128b7
Katsu <karmaelin8@gmail.com>
2022-01-31 20:52:25 +0300

    final 1.1

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,14 +6,24 @@ builder.Services.AddSingleton<ToDoService>();
 
 var app = builder.Build();
 
-app.MapGet("/", async (ToDoService service) => await service.GetAsync());
-app.MapGet("/{id:length(24)}", async (string id, ToDoService service) => await service.GetAsync(id));
+app.MapGet("/", async (ToDoService service) => 
+    await service.GetAsync());
+
+app.MapGet("/{id:length(24)}", async (string id, ToDoService service) => 
+    await service.GetAsync(id) is ToDoItem item 
+    ? Results.Ok(item)
+    : Results.NotFound());
+
 app.MapPost("/", async ([FromBody] ToDoItem item, ToDoService service) => { 
     if (item.Id == null)
         await service.CreateAsync(item);
     else await service.UpdateAsync(item);
-    return Results.Created($"/one/{item.Id}", item);
+    return Results.Created($"/{item.Id}", item);
 });
-app.MapDelete("/{id:length(24)}", async (string id, ToDoService service) => await service.RemoveAsync(id));
+
+app.MapDelete("/{id:length(24)}", async (string id, ToDoService service) => {
+    await service.RemoveAsync(id);
+    return Results.NoContent();
+});
 
 app.Run();
